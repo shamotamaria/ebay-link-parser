09ea7fa0f05ee0e66ee3944ccb7f5b7059923b79
Mari Shamota <marishamota@Marias-MacBook-Pro-2.local>
2023-10-25 17:10:38 -0600

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import asyncio
+import sys
+from helper import compose_url_with_params, parse_element, write_into_file
+
+URL_TO_CRAWL = "https://www.ebay.com/sch/garlandcomputer/m.html"
+FOLDER_NAME = "data"
+CONDITION_PARAM_NAME = "LH_ItemCondition"
+CONDITION_NEW = {CONDITION_PARAM_NAME: 3}
+CONDITION_USED = {CONDITION_PARAM_NAME: 4}
+CONDITION_NOT_SPECIFIED = {CONDITION_PARAM_NAME: 10}
+
+def crawl_url(url):
+  response = requests.get(url)
+  soup = BeautifulSoup(response.content, "html.parser")
+
+  elements = soup.find_all("li", class_="s-item")
+  for element in elements:
+    try:
+      data = parse_element(element)
+      file_name = f"./{FOLDER_NAME}/{data['product_id']}.json"
+      # create async task to write into the file
+      asyncio.create_task(write_into_file(file_name, data))
+    except:
+      print(f"Error happened while processing item {data['product_id']}")
+  #check if there are more pages to parse
+  pagination_next = soup.find("a", class_="pagination__next")
+  if pagination_next:
+    crawl_url(pagination_next['href'])
+
+async def main():
+  params = {}
+  if (len(sys.argv) == 1):
+    crawl_url(URL_TO_CRAWL)
+  else:
+    if (sys.argv[1] == "new"):
+      params = CONDITION_NEW
+      url = compose_url_with_params(URL_TO_CRAWL, params)
+    elif (sys.argv[1] == "used"):
+      params = CONDITION_USED
+      url = compose_url_with_params(URL_TO_CRAWL, params)
+    elif (sys.argv[1] == "notspecified"):
+      params = CONDITION_NOT_SPECIFIED
+      url = compose_url_with_params(URL_TO_CRAWL, params)
+    else:
+      url = URL_TO_CRAWL
+
+    crawl_url(url)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ helper.py
@@ -0,0 +1,39 @@
+import urllib.parse as urlparse
+from urllib.parse import urlencode
+import json
+
+
+async def write_into_file(file_name, data):
+  file_object = open(file_name, "w")
+  file_object.write(json.dumps(data))
+  file_object.close()
+
+def compose_url_with_params(url, params):
+  url_parts = list(urlparse.urlparse(url))
+  query = dict(urlparse.parse_qsl(url_parts[4]))
+  query.update(params)
+  url_parts[4] = urlencode(query)
+  return urlparse.urlunparse(url_parts)
+
+
+def parse_element(element):
+  link = element.find("a", class_="s-item__link")
+  condition = element.find("div", class_="s-item__subtitle")
+  price = element.find("span", class_="s-item__price")
+  product_id = parse_product_id(link['href'])
+
+  data = {}
+  data['product_id'] = product_id
+  data['title'] = link.text if link else ""
+  data['condition'] = condition.text if condition else ""
+  data['price'] = price.text if price else ""
+  data['product_url'] = link['href']
+
+  return data
+
+def parse_product_id(link):
+  # parsing product_id from link https://www.ebay.com/itm/234908325972
+  product_url = link
+  parsed_url = urlparse.urlparse(product_url)
+  url_path = parsed_url.path.split("/")
+  return url_path[2]
